3b2b14237e16e643b06b5abb01b68859aa92239b
Sri Mahi Satti <srimahisatti@gmail.com>
2025-07-09 21:24:18 +0530

    Create chat_bot.py

---
+++ chat_bot.py
@@ -0,0 +1,18 @@
+import openai
+
+openai.api_key = "api-key"
+
+messages = []
+messages.append({"role": "system", "content": "you are a Chat bot Transportation Application"})
+
+print("Hi! I'm your assitant today!! How can I help you?")
+message = "start"
+while message != "quit":
+    message = input()
+    messages.append({"role": "user", "content": message})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages)
+    reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": reply})
+    print("\n" + reply + "\n")
